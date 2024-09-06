a696b46e8afb2c7ecc9306085a5a2f4c4c42bd79
VanceGoblin <zvkubajak@gmail.com>
2024-09-06 16:57:38 -0500

    Added Candidate Interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+export default interface Candidate {
+  image: string;
+  name: string;
+  location: string;
+  email: string;
+  company: string;
+  bio: string;
+};
